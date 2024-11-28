2f3cda6437cadaf3cf0dc40fe887d8f8e506a3f5
sergerad <serge.radinovich@immutable.com>
2024-11-28 19:59:55 +1300

    y2023d1 part 2

--- y2023/d1/main.rs
+++ y2023/d1/main.rs
@@ -3,13 +3,15 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-fn main() -> anyhow::Result<()> {
+use itertools::Itertools;
+
+fn part_1() -> anyhow::Result<()> {
     // Read the input file.
     let file = File::open("./y2023/d1/input.txt")?;
     let reader = BufReader::new(file);
 
     // Parse the lines into a list of 2 digit, base 10 numbers.
-    let sum = reader
+    let nums = reader
         .lines()
         .map(|line| {
             let line = line?;
@@ -25,9 +27,51 @@ fn main() -> anyhow::Result<()> {
             let cat = format!("{}{}", first, second);
             Ok(cat.parse::<u32>()?)
         })
-        .collect::<anyhow::Result<Vec<_>>>();
+        .collect::<anyhow::Result<Vec<_>>>()?;
 
     // Print the sum of the numbers.
-    println!("sum: {:?}", sum?.into_iter().sum::<u32>());
+    println!("part 1: {:?}", nums.into_iter().sum::<u32>());
+    Ok(())
+}
+
+fn part_2() -> anyhow::Result<()> {
+    // Read the input file.
+    let file = File::open("./y2023/d1/input.txt")?;
+    let reader = BufReader::new(file);
+
+    const NUMBERS: &[&str] = &[
+        "0", "zero", "1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six",
+        "7", "seven", "8", "eight", "9", "nine",
+    ];
+
+    let nums = reader
+        .lines()
+        .map(|line| {
+            let line = line?;
+            // Find the first and last instances of a number in the line.
+            let (first, last) = NUMBERS
+                .iter()
+                .enumerate()
+                .flat_map(|(num_index, &num)| {
+                    // Match all instances of the number in the line.
+                    line.match_indices(num)
+                        .map(move |(line_index, _)| (line_index, num_index / 2))
+                })
+                .minmax()
+                .into_option()
+                .unwrap();
+            // Shift first by 1 decimal place and add last.
+            Ok(first.1 * 10 + last.1)
+        })
+        .collect::<anyhow::Result<Vec<_>>>()?;
+
+    // Print the sum of the numbers.
+    println!("part 2: {:?}", nums.into_iter().sum::<usize>());
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    part_1()?;
+    part_2()?;
     Ok(())
 }
