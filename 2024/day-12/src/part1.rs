1ea3110d747e5c8947ab73e3c76b9a92517313df
sergerad <serge.radinovich@immutable.com>
2024-12-13 11:29:44 +1300

    Add Day 12 fixed

--- 2024/day-12/src/part1.rs
+++ 2024/day-12/src/part1.rs
@@ -1,74 +1,43 @@
-use glam::IVec2;
-use nom::{
-    character::complete::{line_ending, satisfy},
-    multi::{many1, separated_list1},
-    IResult,
-};
-use nom_locate::{position, LocatedSpan};
+use petgraph::{algo::condensation, prelude::*, visit::IntoNodeReferences};
+use std::collections::HashMap;
 
-pub type Span<'a> = LocatedSpan<&'a str>;
-
-fn parse_char_pos(input: Span) -> IResult<Span, (IVec2, char)> {
-    let (input, pos) = position(input)?;
-    let x = pos.get_column() as i32 - 1;
-    let y = pos.location_line() as i32 - 1;
-    let (input, c) = satisfy(|c| c.is_alphabetic())(input)?;
-    Ok((input, (IVec2::new(x, y), c)))
-}
-
-fn parse(input: Span) -> IResult<Span, Vec<Vec<(IVec2, char)>>> {
-    separated_list1(line_ending, many1(parse_char_pos))(input)
-}
 #[tracing::instrument]
-pub fn process(input: &'static str) -> miette::Result<String> {
-    let (_, matrix) = parse(Span::new(input)).map_err(|e| miette::miette!(e))?;
-    let rows = matrix.len();
-    let cols = matrix[0].len();
-    //for row in matrix {
-    //    for (pos, c) in row {
-    //        print!("{c}");
-    //    }
-    //    println!("")
-    //}
-    let mut chunks = matrix.iter().flatten().collect::<Vec<_>>();
-    chunks.sort_by(|&a, &b| a.1.cmp(&b.1));
-    let sum = chunks
-        .chunk_by(|&a, &b| a.1 == b.1)
-        .map(|blocks| {
-            let block_sum = blocks
-                .iter()
-                .map(|(pos, c)| {
-                    [
-                        IVec2::new(-1, 0),
-                        IVec2::new(1, 0),
-                        IVec2::new(0, -1),
-                        IVec2::new(0, 1),
-                    ]
-                    .into_iter()
-                    .map(|direction| {
-                        let new_pos = *pos + direction;
-                        if new_pos.x < 0
-                            || new_pos.y < 0
-                            || new_pos.x >= cols as i32
-                            || new_pos.y >= rows as i32
-                        {
-                            1
-                        } else {
-                            if matrix[new_pos.y as usize][new_pos.x as usize].1 == *c {
-                                0
-                            } else {
-                                1
-                            }
-                        }
-                    })
-                    .sum::<i32>()
-                })
-                .sum::<i32>();
-            let block_len = blocks.len() as i32;
-            println!("block {}: {block_sum}, block_len: {block_len}", blocks[0].1);
-            block_sum * block_len
+pub fn process(input: &str) -> miette::Result<String> {
+    let blocks = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, c)| ((x as i32, y as i32), c))
         })
-        .sum::<i32>();
+        .collect::<HashMap<(i32, i32), char>>();
+
+    let mut block_graph: UnGraphMap<(i32, i32), ()> = UnGraphMap::new();
+
+    for ((x, y), c) in blocks.iter() {
+        let block = block_graph.add_node((*x, *y));
+
+        for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)].iter() {
+            let next_block = (x + direction.0, y + direction.1);
+            if blocks.get(&next_block).is_some_and(|next_c| c == next_c) {
+                block_graph.add_edge(block, next_block, ());
+            };
+        }
+    }
+
+    let condensed_graph = condensation(block_graph.clone().into_graph::<NodeIndex>(), false);
+    let sum = condensed_graph
+        .node_references()
+        .map(|(_, neighbours)| {
+            let perimeter = neighbours
+                .iter()
+                .map(|neighbour| 4 - block_graph.neighbors(*neighbour).count())
+                .sum::<usize>();
+            neighbours.len() * perimeter
+        })
+        .sum::<usize>();
+
     Ok(sum.to_string())
 }
 
@@ -88,10 +57,6 @@ VVIIICJJEE
 MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE";
-        //        let input = "AAAA
-        //BBCD
-        //BBCC
-        //EEEC";
         assert_eq!("1930", process(input)?);
         Ok(())
     }
--- 2024/day-12/src/part2.rs
+++ 2024/day-12/src/part2.rs
@@ -1,6 +1,69 @@
+use itertools::Itertools;
+use petgraph::{algo::condensation, prelude::*, visit::IntoNodeReferences};
+use std::collections::HashMap;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let blocks = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, c)| ((x as i32, y as i32), c))
+        })
+        .collect::<HashMap<(i32, i32), char>>();
+
+    let mut block_graph: UnGraphMap<(i32, i32, char), ()> = UnGraphMap::new();
+
+    for ((x, y), c) in blocks.iter() {
+        let block = block_graph.add_node((*x, *y, *c));
+
+        for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)].iter() {
+            let next_block = (x + direction.0, y + direction.1);
+            if blocks.get(&next_block).is_some_and(|&next_c| *c == next_c) {
+                block_graph.add_edge(block, (next_block.0, next_block.1, *c), ());
+            };
+        }
+    }
+
+    let condensed_graph = condensation(block_graph.clone().into_graph::<NodeIndex>(), false);
+    let sum = condensed_graph
+        .node_references()
+        .map(|(_, neighbours)| {
+            neighbours
+                .iter()
+                .map(|neighbour| {
+                    [(0, 1), (1, 0), (0, -1), (-1, 0)]
+                        .iter()
+                        .circular_tuple_windows()
+                        .fold(0, |sum, (a, b)| {
+                            let a_is_neighbour = blocks
+                                .get(&(a.0 + neighbour.0, a.1 + neighbour.1))
+                                .is_some_and(|c| *c == neighbour.2);
+                            let b_is_neighbour = blocks
+                                .get(&(b.0 + neighbour.0, b.1 + neighbour.1))
+                                .is_some_and(|c| *c == neighbour.2);
+                            let a_b_is_not_neighbour = blocks
+                                .get(&(a.0 + b.0 + neighbour.0, a.1 + b.1 + neighbour.1))
+                                .is_some_and(|c| *c != neighbour.2);
+                            if a_is_neighbour && b_is_neighbour && a_b_is_not_neighbour {
+                                // have interior corner
+                                sum + 1
+                            } else if !a_is_neighbour && !b_is_neighbour {
+                                //have exterior corner
+                                sum + 1
+                            } else {
+                                sum
+                            }
+                        })
+                })
+                .sum::<usize>()
+                * neighbours.len()
+        })
+        .sum::<usize>();
+
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +72,17 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE";
+        assert_eq!("1206", process(input)?);
         Ok(())
     }
 }
